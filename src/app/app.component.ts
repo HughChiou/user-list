d76765504ea02d017f2e96565257c5e2ce84ef86
Hugh C <hughchiou@gmail.com>
2022-10-30 01:08:33 +0000

    bind user data to table layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { parse, parseJSON } from 'date-fns';
+import { BehaviorSubject } from 'rxjs';
+import { USERS } from 'src/json/users';
+import { User } from 'src/model/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'user-list';
+  userList$ = new BehaviorSubject<Nullable<User[]>>(null);
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const users: User[] = USERS.map((item) => new User(item));
+
+    this.userList$.next(users);
+  }
+
+
 }
+
+type Nullable<T> = T | null;
+
+
---
+++ src/model/user.ts
@@ -0,0 +1,24 @@
+import { parseJSON } from 'date-fns';
+export class User {
+  name: string;
+  age: number;
+  registered: Date;
+  email: string;
+  balance: number;
+
+  constructor(input: unknown) {
+    this.name = (input as { name: string }).name;
+    this.age = (input as { age: number }).age;
+    this.registered = this.parseJSONDate((input as { registered: string }).registered);
+    this.email = (input as { email: string }).email;
+    this.balance = this.parseCurrency((input as { balance: string }).balance);
+  }
+
+  private parseCurrency(currency: string): number {
+    return Number(currency.replace(/[^0-9.-]+/g, ""))
+  }
+
+  private parseJSONDate(date: string): Date {
+    return parseJSON(date);
+  }
+}
